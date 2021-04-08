ed66a9c6ee9fed167fab0ace3ed546778031525f
sergey-as <sergey_as@ukr.net>
2021-04-08 18:42:45 +0300

    react_lesson3_2012_03_29  12345

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React, {Component} from "react";
+// import React from "react";
+import React, {Component, useEffect, useState} from "react";
 import './App.css';
 import Comp from "./components/comp/Comp";
 
+const url = 'https://jsonplaceholder.typicode.com/todos';
 
+const App = () => {
+    const [counter, setCounter] = useState(0);
+    const [todos, setTodos] = useState([]);
+
+    const incCounter = () => {
+        setCounter(counter + 1);
+    }
+
+    const fetchTodos = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setTodos(data);
+        console.log(data);
+    }
+
+    useEffect(() => {
+        fetchTodos();
+    }, [])
+
+    return (
+        <>
+            <h1 onClick={incCounter}>HELLO react {counter}</h1>
+            {!!todos.length && (
+                <>
+                    <hr/>
+                    <h3>{todos[0].title} - {todos[0].completed.toString()}</h3>
+                    <hr/>
+                </>
+            )}
+        </>
+    )
+}
+
+
+// 1 class
 // class App extends Component {
 //     state = {
 //         counter: 0
@@ -33,34 +70,35 @@ import Comp from "./components/comp/Comp";
 //     }
 // }
 
-const App = () => {
-    const [counter, setCounter] = React.useState(0);
-
-    const incCounter = () => {
-        setCounter(counter + 1);
-    }
-
-
-    React.useEffect(() => {
-        console.log('did mount parent');
-    }, []);
-
-    React.useEffect(() => {
-        //2
-        console.log('did update parent');
-
-        //1
-        return () => console.log('parent unmount');
-    }, [counter]);
-
-    return (
-        <>
-            <h1 onClick={incCounter}>HELLO react {counter}</h1>
-            {!!(counter % 2) && <Comp/>}
-        </>
-
-    )
-}
+// 2 function
+// const App = () => {
+//     const [counter, setCounter] = React.useState(0);
+//
+//     const incCounter = () => {
+//         setCounter(counter + 1);
+//     }
+//
+//
+//     React.useEffect(() => {
+//         console.log('did mount parent');
+//     }, []);
+//
+//     React.useEffect(() => {
+//         //2
+//         console.log('did update parent');
+//
+//         //1
+//         return () => console.log('parent unmount');
+//     }, [counter]);
+//
+//     return (
+//         <>
+//             <h1 onClick={incCounter}>HELLO react {counter}</h1>
+//             {!!(counter % 2) && <Comp/>}
+//         </>
+//
+//     )
+// }
 
 // function App() {
 //   return (
